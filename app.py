b5564c238b7eb8697654d9df603db87b1db8bb0d
Eliotboutet <bouteteliot@gmail.com>
2022-02-10 17:47:50 +0100

    relations

--- app.py
+++ app.py
@@ -9,9 +9,9 @@ app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database/database.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
-db.init_app(app)  # (1) flask prend en compte la base de donnee
+db.init_app(app)
+
 with app.test_request_context():
-    # (2) bloc exécuté à l'initialisation de Flask
     init_database()
 
 
